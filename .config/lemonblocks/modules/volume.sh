f94c1202afce479d61ff833d8cd05fe417c98288
Vidhu Kant Sharma <bokuwakanojogahoshii@yahoo.com>
2021-04-08 16:38:30 +0530

    enabled golang and other modules in starship, trying to push to this repo after messing everything up

--- .config/lemonblocks/modules/volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf " VOL:$(pamixer --get-volume-human)% "
-# if [ $volume_level == "muted" ]; then
-        # printf "%{F#ff0058}VOL: $(pamixer --get-volume)%{F-}"
-# else
-        # printf "VOL: $volume_level"
-# fi
-
