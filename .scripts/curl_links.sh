a9a5f3284893155c8d2e406366d00e24ec91032c
Vidhu Kant Sharma <bokuwakanojogahoshii@yahoo.com>
2021-04-12 21:32:04 +0530

    pushing a script to curl multiple items (pitcures)

---
+++ .scripts/curl_links.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+output=1
+ext="gif" # without the dot
+for i in $(cat $ext.txt); do
+	curl "{$i}" --output /zt/Docs/pics/$output$ext
+	# echo "{$i} /pics/$output.$ext"
+	((output=$output+1))
+done
