19b77d0b11b3a4b7688f0c0009e29528432e5667
Vidhu Kant Sharma <bokuwakanojogahoshii@yahoo.com>
2021-04-12 21:34:30 +0530

    fixed file extention error

--- .scripts/curl_links.sh
+++ .scripts/curl_links.sh
@@ -3,7 +3,7 @@
 output=1
 ext="gif" # without the dot
 for i in $(cat $ext.txt); do
-	curl "{$i}" --output /zt/Docs/pics/$output$ext
+	curl "{$i}" --output /zt/Docs/pics/$output.$ext
 	# echo "{$i} /pics/$output.$ext"
 	((output=$output+1))
 done
